6da36d835073e9d2168b92b7a2fd6444067c68e0
Nel-Tu-Caporal <n.caporal.146050.tc@umindanao.edu.ph>
2025-12-02 23:10:13 +0800

    Initial commit

---
+++ HotelReservationAndManagementSystem/Model/FormLogIn.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace HotelReservationAndManagementSystem
+{
+    partial class FormLogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogIn));
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.pictureBoxShow = new System.Windows.Forms.PictureBox();
+            this.labelForgotPassword = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonLogIn = new System.Windows.Forms.Button();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.pictureBoxHide = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PictureBoxMinimize = new System.Windows.Forms.PictureBox();
+            this.PictureBoxClose = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxShow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxMinimize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 648);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1366, 72);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "© 2025 All rights reserved.";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.textBoxPassword);
+            this.groupBox1.Controls.Add(this.pictureBoxShow);
+            this.groupBox1.Controls.Add(this.labelForgotPassword);
+            this.groupBox1.Controls.Add(this.labelPassword);
+            this.groupBox1.Controls.Add(this.buttonLogIn);
+            this.groupBox1.Controls.Add(this.textBoxUserName);
+            this.groupBox1.Controls.Add(this.labelUserName);
+            this.groupBox1.Controls.Add(this.pictureBoxHide);
+            this.groupBox1.Location = new System.Drawing.Point(156, 143);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(422, 379);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Please LogIn First";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxPassword.Location = new System.Drawing.Point(62, 189);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(286, 32);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPassword_TextChanged);
+            // 
+            // pictureBoxShow
+            // 
+            this.pictureBoxShow.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBoxShow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxShow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxShow.Image = global::HotelReservationAndManagementSystem.Properties.Resources.eye;
+            this.pictureBoxShow.Location = new System.Drawing.Point(352, 189);
+            this.pictureBoxShow.Name = "pictureBoxShow";
+            this.pictureBoxShow.Size = new System.Drawing.Size(35, 32);
+            this.pictureBoxShow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxShow.TabIndex = 4;
+            this.pictureBoxShow.TabStop = false;
+            this.pictureBoxShow.Click += new System.EventHandler(this.pictureBoxShow_Click);
+            this.pictureBoxShow.MouseHover += new System.EventHandler(this.pictureBoxShow_MouseHover);
+            // 
+            // labelForgotPassword
+            // 
+            this.labelForgotPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelForgotPassword.AutoSize = true;
+            this.labelForgotPassword.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelForgotPassword.Location = new System.Drawing.Point(138, 265);
+            this.labelForgotPassword.Name = "labelForgotPassword";
+            this.labelForgotPassword.Size = new System.Drawing.Size(178, 23);
+            this.labelForgotPassword.TabIndex = 0;
+            this.labelForgotPassword.Text = "Forgot Password?";
+            this.labelForgotPassword.Click += new System.EventHandler(this.labelForgotPassword_Click);
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.Location = new System.Drawing.Point(58, 151);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(106, 23);
+            this.labelPassword.TabIndex = 3;
+            this.labelPassword.Text = "Password:";
+            this.labelPassword.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // buttonLogIn
+            // 
+            this.buttonLogIn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonLogIn.Location = new System.Drawing.Point(51, 291);
+            this.buttonLogIn.Name = "buttonLogIn";
+            this.buttonLogIn.Size = new System.Drawing.Size(342, 56);
+            this.buttonLogIn.TabIndex = 3;
+            this.buttonLogIn.Text = "Log In";
+            this.buttonLogIn.UseVisualStyleBackColor = true;
+            this.buttonLogIn.Click += new System.EventHandler(this.buttonLogIn_Click);
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxUserName.Location = new System.Drawing.Point(62, 103);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(331, 32);
+            this.textBoxUserName.TabIndex = 2;
+            this.textBoxUserName.TextChanged += new System.EventHandler(this.textBoxUserName_TextChanged);
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelUserName.AutoSize = true;
+            this.labelUserName.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUserName.Location = new System.Drawing.Point(58, 67);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(114, 23);
+            this.labelUserName.TabIndex = 0;
+            this.labelUserName.Text = "Username:";
+            this.labelUserName.Click += new System.EventHandler(this.labelUserName_Click);
+            // 
+            // pictureBoxHide
+            // 
+            this.pictureBoxHide.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBoxHide.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxHide.Image = global::HotelReservationAndManagementSystem.Properties.Resources.eye_;
+            this.pictureBoxHide.Location = new System.Drawing.Point(352, 189);
+            this.pictureBoxHide.Name = "pictureBoxHide";
+            this.pictureBoxHide.Size = new System.Drawing.Size(35, 32);
+            this.pictureBoxHide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxHide.TabIndex = 5;
+            this.pictureBoxHide.TabStop = false;
+            this.pictureBoxHide.Click += new System.EventHandler(this.pictureBoxHide_Click);
+            this.pictureBoxHide.MouseHover += new System.EventHandler(this.pictureBoxHide_MouseHover);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Location = new System.Drawing.Point(722, 130);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(8, 448);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HotelReservationAndManagementSystem.Properties.Resources.reception;
+            this.pictureBox1.Location = new System.Drawing.Point(840, 187);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(471, 368);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PictureBoxMinimize
+            // 
+            this.PictureBoxMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PictureBoxMinimize.Image = global::HotelReservationAndManagementSystem.Properties.Resources.minimize_btn;
+            this.PictureBoxMinimize.Location = new System.Drawing.Point(1219, 7);
+            this.PictureBoxMinimize.Name = "PictureBoxMinimize";
+            this.PictureBoxMinimize.Size = new System.Drawing.Size(36, 36);
+            this.PictureBoxMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBoxMinimize.TabIndex = 1;
+            this.PictureBoxMinimize.TabStop = false;
+            this.PictureBoxMinimize.Click += new System.EventHandler(this.PictureBoxMinimize_Click);
+            this.PictureBoxMinimize.MouseHover += new System.EventHandler(this.pictureBoxMinimize_MouseHover);
+            // 
+            // PictureBoxClose
+            // 
+            this.PictureBoxClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PictureBoxClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureBoxClose.Image = global::HotelReservationAndManagementSystem.Properties.Resources.exit_btn;
+            this.PictureBoxClose.Location = new System.Drawing.Point(1265, 7);
+            this.PictureBoxClose.Name = "PictureBoxClose";
+            this.PictureBoxClose.Size = new System.Drawing.Size(35, 35);
+            this.PictureBoxClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBoxClose.TabIndex = 0;
+            this.PictureBoxClose.TabStop = false;
+            this.PictureBoxClose.Click += new System.EventHandler(this.pictureBoxClose_Click);
+            this.PictureBoxClose.MouseHover += new System.EventHandler(this.pictureBoxClose_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(860, 506);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(451, 49);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hotel Reservation and";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(860, 558);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(440, 49);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Management System";
+            
+            // 
+            // FormLogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1366, 720);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PictureBoxMinimize);
+            this.Controls.Add(this.PictureBoxClose);
+            this.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FormLogIn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormLogIn";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormLogIn_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxShow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxMinimize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxClose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBoxClose;
+        private System.Windows.Forms.PictureBox PictureBoxMinimize;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonLogIn;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.Label labelUserName;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelForgotPassword;
+        private System.Windows.Forms.PictureBox pictureBoxShow;
+        private System.Windows.Forms.PictureBox pictureBoxHide;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ HotelReservationAndManagementSystem/Model/FormLogIn.cs
@@ -0,0 +1,126 @@
+﻿using AMRConnector;
+using HotelReservationAndManagementSystem.References;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace HotelReservationAndManagementSystem
+{
+    public partial class FormLogIn : Form
+    {
+
+        DBConnector1 db;
+        public FormLogIn()
+        {
+            InitializeComponent();
+
+            db = new DBConnector1();
+
+          
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void FormLogIn_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBoxMinimize_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(PictureBoxMinimize, "Minimize");
+        }
+
+        private void pictureBoxClose_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(PictureBoxClose, "Close");
+        }
+
+        private void PictureBoxMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void buttonLogIn_Click(object sender, EventArgs e)
+        {
+            bool check = db.IsValidNamePass(textBoxUserName.Text.Trim(), textBoxPassword.Text.Trim());
+
+            if (textBoxUserName.Text.Trim() == string.Empty || textBoxPassword.Text.Trim() == string.Empty)
+                MessageBox.Show("Please fill out all fields.", "Required field", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            else
+            {
+                if (check)
+                {
+                    FormDashboard fd = new FormDashboard();
+                    fd.Show();
+                }
+                else
+
+                    MessageBox.Show("Invalid User name or password", "Username or password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+            
+        private void textBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelForgotPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelUserName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBoxShow_Click(object sender, EventArgs e)
+        {
+            pictureBoxShow.Hide();
+            textBoxPassword.UseSystemPasswordChar =false; 
+            pictureBoxHide.Show();
+        }
+
+        private void pictureBoxShow_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(pictureBoxShow, "Show Password");
+        }
+
+        private void pictureBoxHide_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(pictureBoxHide, "Hide Password");
+        }
+
+        private void pictureBoxHide_Click(object sender, EventArgs e)
+        {
+            pictureBoxHide.Hide();
+            textBoxPassword.UseSystemPasswordChar = true;
+            pictureBoxShow.Show();
+        }
+
+        
+    }
+}
